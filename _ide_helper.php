3e0c2658cd7952c3208d3e7ce012de9c55092334
Mo Khosh <mskhoshnazar@gmail.com>
2024-08-16 15:17:18 +0330

    add ide helper for auto completion

---
+++ _ide_helper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Filament\Forms\Components {
+    /**
+     * @method static \Filament\Forms\Components\DateTimePicker jalali(bool $ignore = false)
+     */
+    class DateTimePicker {}
+}
+
+namespace Filament\Tables\Columns {
+    /**
+     * @method static \Filament\Tables\Columns\TextColumn jalaliDate(?string $format = null, ?string $timezone = null, bool $ignore = false)
+     * @method static \Filament\Tables\Columns\TextColumn jalaliDateTime(?string $format = null, ?string $timezone = null, bool $ignore = false)
+     */
+    class TextColumn {}
+}
+
+namespace Filament\Infolists\Components {
+    /**
+     * @method static \Filament\Infolists\Components\TextEntry jalaliDate(?string $format = null, ?string $timezone = null, bool $ignore = false)
+     * @method static \Filament\Infolists\Components\TextEntry jalaliDateTime(?string $format = null, ?string $timezone = null, bool $ignore = false)
+     */
+    class TextEntry {}
+}
