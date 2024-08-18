ab965d91f3f75d6786cac6c0d2a4e420b5cd2b3a
mokhosh <mokhosh@users.noreply.github.com>
2024-08-18 09:04:39 +0000

    Fix styling

--- src/FilamentJalaliServiceProvider.php
+++ src/FilamentJalaliServiceProvider.php
@@ -35,7 +35,7 @@ class FilamentJalaliServiceProvider extends PackageServiceProvider
     protected function getAssets(): array
     {
         return [
-            AlpineComponent::make('filament-jalali', __DIR__ . '/../resources/js/dist/components/filament-jalali.js'),
+            AlpineComponent::make('filament-jalali', __DIR__.'/../resources/js/dist/components/filament-jalali.js'),
         ];
     }
 
