3b432567c530824d656272190b34e4362ea77e67
Mo Khosh <mskhoshnazar@gmail.com>
2024-10-21 16:29:35 +0330

    add test for default format

--- tests/Tests/FilamentJalaliTest.php
+++ tests/Tests/FilamentJalaliTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
 use Illuminate\Support\Carbon;
 
 it('formats state to jalali date', function () {
@@ -8,5 +9,14 @@ it('formats state to jalali date', function () {
         ->jalaliDate()
         ->formatState(Carbon::parse('1989-10-07'))
         ->toBe('مهر 15, 1368');
+});
+
+it('formats state based on default date display format', function () {
+    Table::$defaultDateDisplayFormat = 'Y-m-d';
+
+    expect(TextColumn::make(''))
+        ->jalaliDate()
+        ->formatState(Carbon::parse('1989-10-07'))
+        ->toBe('1368-07-15');
 });
 
